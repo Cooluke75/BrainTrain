50a583f6d275e906e6bfa157dc23507f3a639125
Kabir Cahill <kabir_cahill@hotmail.com>
2016-05-09 18:16:53 -0700

    Added grid.

---
+++ grid.js
@@ -0,0 +1,71 @@
+/*
+	grid.js
+	description: 	Builds the gameboard for BrainTrain game.
+	authors: 		Kabir Cahill, Kent Huang, Luke Lee, Eric Lin, Roger Zhang
+*/
+
+var rows = 9;
+var cols = 16;
+//var activeTracks = [][];
+
+$(document).ready(function(){
+	buildGrid();
+	resizeGrid();
+	
+	// resize grid when window is resized
+	$(window).resize(resizeGrid);
+	
+	var tile1 = $('.tile:first-of-type');
+	tile1.css('backgroundColor', '#009933');
+	tile1.css('position', 'relative');
+	$('#grid').click(function() {
+		tile1.animate({left: '+=' + tile1.outerWidth() + 'px'});
+	});
+});
+
+// Adds the tiles to the grid
+function buildGrid() {
+	// clear the grid
+	$('#grid').empty();
+	
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < cols; j++) {
+			$("#grid").append('<div class="tile">'/* + i + ', ' + j */+ '</div>');			
+		}
+	}
+}
+	
+// Scales the grid to fit the screen
+function resizeGrid() {
+	// fit grid in window
+	$('#grid').width('80%');
+	$('#grid').height('80%');
+	
+	// get grid width and height in px
+	var gridWidth = $('#grid').width();
+	var gridHeight = $('#grid').height();
+	var tileWidth;
+	var tileHeight;
+	
+	// choose tile size that fits both grid width and height
+	if((gridWidth / cols) < (gridHeight / rows)) {
+		tileWidth = gridWidth / cols;
+		tileHeight = tileWidth;
+	} else {
+		tileHeight = gridHeight / rows;
+		tileWidth = tileHeight;
+	}
+
+	// set tile width and height
+	$('.tile').outerHeight(tileHeight);
+	$('.tile').outerWidth(tileWidth);
+	
+	// adjust grid width and height
+	$('#grid').width(tileWidth * cols);
+	$('#grid').height(tileHeight * rows);
+}
+
+// Generates the track
+function buildTrack(level) {
+	
+}
