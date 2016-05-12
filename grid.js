37bd688591f80d2a8102b4ddf16e4074a29b4f9c
Kabir Cahill <kabir_cahill@hotmail.com>
2016-05-11 18:06:48 -0700

    The user's answers are now validated and their total score is updated.

--- grid.js
+++ grid.js
@@ -9,6 +9,8 @@ var cols = 16;
 var grid;
 var track; // A 2D array representing the train track. True means the tile is part of the track.
 var level = 0;
+var totalScore = 0;
+var levelComplete = false;
 var difficultyTier = 3;
 var numberOfTrains = 2;
 var startingPoints = [];
@@ -23,40 +25,87 @@ $(document).ready(function(){
 	// resize grid when window is resized
 	$(window).resize(resizeGrid);
 
-    // the user has chosen a destination
-    $('.destButton').click(function () {
-        // get the row of the button
-        var row = parseInt($(this).attr('id').replace(/\D/g, ''));
-
-        // get the index of the row in userAnswers
-         var answerIndex = usersAnswers.findIndex(function (userAnswer) { return userAnswer == row; });
-
-        // if the row has not already been stored, add it to the array
-        if(answerIndex == -1) {
-            // make the button 'selected' (i.e. change its color)
-            $(this).css('backgroundColor', 'yellow');
-            usersAnswers.push(row); // store the button's row in the array of user's answers
-        }
-
-        var soundEffect = new Audio();
-        soundEffect.src = "Correct.mp3";
-        soundEffect.play();
-
-
-        // when the length of the answers array == number of trains, validate each answer ( with a function)
-    });
-
+    // place the trains at their starting position
     startingPoints = randomTrains(difficultyTier,numberOfTrains);
     for(i = 0; i < startingPoints.length; i++) {
         placeTheTrain(startingPoints[i]);
     }
-    grid.click(function(){
-        for(i = 0; i < startingPoints.length; i++) {
-            moveTheTrain(startingPoints[i]);
+
+    // determine the correct destinations
+    correctAnswers = giveTheAnswerArr(startingPoints);
+
+    // the user has chosen a destination
+    $('.destButton').click(function () {
+
+        if (usersAnswers.length < numberOfTrains) {
+
+            // get the row of the button
+            var row = parseInt($(this).attr('id').replace(/\D/g, ''));
+
+            // get the index of the row in userAnswers
+            var answerIndex = usersAnswers.findIndex(function (userAnswer) {
+                return userAnswer == row;
+            });
+
+            // if the row has not already been stored, add it to the array
+            if (answerIndex == -1) {
+                // make the button 'selected' (i.e. change its color)
+                //$(this).css('backgroundColor', 'yellow');
+                usersAnswers.push(row); // store the button's row in the array of user's answers
+            }
+
+            var soundEffect = new Audio();
+
+            // get the index of the row in correctAnswers
+            answerIndex = correctAnswers.findIndex(function (correctAnswer) {
+                return correctAnswer == row;
+            });
+
+            // if the row is a correct destination, add to the users score, otherwise subtract
+            if (answerIndex != -1) {
+                // add 100 points from user's score
+                totalScore += 100;
+                soundEffect.src = "Correct.mp3";
+                $(this).css('backgroundColor', '#00ff00');
+            } else {
+                // deduct 50 points from user's score
+                totalScore -= 50;
+                soundEffect.src = "Wrong.mp3";
+                $(this).css('backgroundColor', 'black');
+            }
+
+            soundEffect.play();
+
+            if (usersAnswers.length == numberOfTrains) {
+                // move the trains
+                for(i = 0; i < startingPoints.length; i++) {
+                    moveTheTrain(startingPoints[i]);
+                }
+
+                usersAnswers.sort();
+                correctAnswers.sort();
+
+                if(usersAnswers.isEqual(correctAnswers)) {
+                    levelComplete = true;
+                    console.log('You win! Score: ' + totalScore);
+                } else {
+                    console.log('You lose. Score: ' + totalScore);
+                }
+            }
         }
     });
-
 });
+
+// An array method that compares if two arrays are equal (same length, values and order)
+Array.prototype.isEqual = function (otherArray) {
+    if(this.length != otherArray.length) {
+        return false;
+    }
+
+    return this.every(function (value, i) {
+        return value == otherArray[i];
+    });
+}
 
 // Adds the tiles to the grid
 function buildGrid() {
@@ -121,5 +170,5 @@ function resizeGrid() {
 
 // Generates the track
 function buildTrack(level) {
-	
+
 }
