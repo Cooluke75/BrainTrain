0d5c85b92d731a44175f82efc0c6a43624b751fa
Kabir Cahill <kabir_cahill@hotmail.com>
2016-05-25 02:27:00 -0700

    Add functions to upload username to users table and to check if a username is unique.

--- Javascript/Username.js
+++ Javascript/Username.js
@@ -4,6 +4,10 @@
  authors:        Kabir Cahill, Kent Huang, Luke Lee, Eric Lin, Roger Zhang
  */
 
+// get the URL of the users table
+var usersTableURL = 'https://api.mlab.com/api/1/databases/braintrain/collections/users'
+    + '?apiKey=SWfr016sOh1qeUqTCZuHb7O9IMMDgBby';
+
 /**
  * Stores the user's username to local storage.
  * @param key
@@ -76,4 +80,51 @@ $(document).ready(function(){
     $("#save").click(saveUserName);
     $("#change").click(changeTheName);
 });
-  
+
+
+/**
+ * Sends the user's username to the database.
+ */
+function sendUsernameToDatabase(username) {
+    var titleToBeSent = 'Noob';
+
+    //send the data
+    $.ajax({
+        url: usersTableURL,
+        data: JSON.stringify( { "username" : username, "title": titleToBeSent } ),
+        type: "POST",
+        contentType: "application/json",
+        success: function(){
+            // Notify user that their username has been saved <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        },
+        error: function (xhr) {
+            console.log('Error: ' + xhr.status + ' ' + xhr.statusText + ' ' + xhr.responseText);
+        }
+    });
+}
+
+/**
+ * Checks if the username given is unique (i.e. it doesn't exist in the database).
+ */
+function isUniqueUsername(username) {
+    var isUnique = false;
+
+    // check if there is an occurrence of the username
+    query = '&q={"username": '+ username + '}&c=true';
+
+    $.ajax({
+        url: usersTableURL + query,
+        success: function (result) {
+            if (result > 0) {
+                isUnique = false;
+            } else {
+                isUnique = true;
+            }
+
+            return isUnique;
+        },
+        error: function (xhr) {
+            console.log('Error: ' + xhr.status + ' ' + xhr.statusText + ' ' + xhr.responseText);
+        }
+    });
+}
